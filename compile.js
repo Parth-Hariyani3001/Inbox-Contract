3a665311ea68170f0407be5ebc1605e185892b47
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2023-02-12 01:54:46 +0530

    Added a comment in compile.js

--- compile.js
+++ compile.js
@@ -1,5 +1,5 @@
 // compile code will go here
-const path = require('path');
+const path = require('path');   //We use "path" module as "require" module will try to compile our solidity code
 const fs = require('fs');
 const solc = require('solc');
 
